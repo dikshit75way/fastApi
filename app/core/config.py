5a067d2a6e2a93dad2f716364842e5ddb01b4ec8
dikshit75way <dikshit.75way@gmail.com>
2026-01-06 19:11:47 +0530

    changed

--- app/core/config.py
+++ app/core/config.py
@@ -12,13 +12,13 @@ class Settings(BaseSettings):
     
     # JWT Settings
     SECRET_KEY: str
+
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     # Stripe Settings
-    # STRIPE_SECRET_KEY: str = "sk_test_51SVWMlFlnwfqrHjipgcnZaQstqhHD9dHHp2HrP3QKozZ62yU8JIBp8tagAS2yEyJmr0ksGbW7naJF3TwsOXeilV100UNrp1MQ1"
-    # STRIPE_WEBHOOK_SECRET: str = "whsec_754a328554d444d76a3312bd20addb47c8a1b519d01ac9f4cbb7c5e5773f7672"
+    
     STRIPE_SECRET_KEY: str = os.getenv("STRIPE_SECRET_KEY")
     STRIPE_WEBHOOK_SECRET: str = os.getenv("STRIPE_WEBHOOK_SECRET")
 
